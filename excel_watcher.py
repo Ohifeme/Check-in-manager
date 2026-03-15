831a1d35e4be245714a3f8ccae53b61af8d270ac
Ohifeme Unuigboje <ohifemeunuigboje@femes-Mac.local>
2026-03-14 20:43:38 -0400

    demo run with printing netids

--- excel_watcher.py
+++ excel_watcher.py
@@ -2,32 +2,37 @@ import pandas as pd
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-FILE = "scanner_output.xlsx"
+FILE = "Cookout_F25.xlsx"
 
 class ExcelHandler(FileSystemEventHandler):
 
     def process_existing_rows(self):
+        print(f"Printing pre exisitng data from {FILE}...\n")
         df = pd.read_excel(FILE)
-
+        valid_no=0
         for _, row in df.iterrows():
-            netid = row["D"] #read from column G
-            process_netid(netid)
+            netid = row.iloc[3] 
+            if type(netid)is str:
+                valid_no+=1
+                process_netid(netid,valid_no)
 
     def on_modified(self, event):
 
-        if event.src_path.endswith("scanner_output.xlsx"):
+        if event.src_path.endswith("Cookout_F25.xlsx"):
 
             df = pd.read_excel(FILE)
 
             last_row = df.iloc[-1]
-            netid = last_row["D"] #read from column G
+            netid = last_row["D"] #read from column D
 
             process_netid(netid)
 
-def process_netid(netid):
-    print("New scan:", netid)
+def process_netid(netid,index=None):
+    print(index," New scan:", netid)
 
 handler = ExcelHandler()
+handler.process_existing_rows()
+
 observer = Observer()
 observer.schedule(handler, ".", recursive=False)
 
